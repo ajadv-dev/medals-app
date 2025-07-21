43a927635388dcd30382fba34db4396d446ed327
Abhijeet Jadhav <abhijeetj26@gmail.com>
2025-07-21 21:40:14 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
-    output: 'export',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    output: "export",
+    basePath,
+    assetPrefix: isProd ? `${basePath}/` : "",
     images: {
         unoptimized: true,
     },
