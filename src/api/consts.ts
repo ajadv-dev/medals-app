d412923431a84ff9f8ad9bf89b953fa379d29c73
ajadv-dev <abhijeetj26@gmail.com>
2025-07-21 22:06:22 +0200

    check image path

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -1,3 +1,3 @@
-export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
+export const BASE_PATH = (process.env.NEXT_PUBLIC_BASE_PATH || '').trim();
 export const medalsApiUrl = `${BASE_PATH}/mock/medals.json`;
 export const flagsPngUrl = `${BASE_PATH}/flags.png`;
